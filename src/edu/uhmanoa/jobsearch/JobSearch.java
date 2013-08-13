1e933d5cbb3244e8d0d5320aa2a7b355f5272a38
Erika Nana <enana@hawaii.edu>
2013-08-13 02:23:42 -1000

    User is able to login

--- src/edu/uhmanoa/jobsearch/JobSearch.java
+++ src/edu/uhmanoa/jobsearch/JobSearch.java
@@ -8,24 +8,30 @@ import org.jsoup.nodes.Document;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class JobSearch extends Activity implements OnClickListener{
-	TextView view;
-	Button button;
-	String cookie;
+	
+	Button mLoginButton;
+	EditText mUserName;
+	EditText mPassword;
+	
 	public static final String LOGIN_URL = "https://sece.its.hawaii.edu/sece/stdLogin.do";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_job_search);
-		button = (Button) findViewById(R.id.button);
-		button.setOnClickListener(this);
+		mLoginButton = (Button) findViewById(R.id.loginButton);
+		mLoginButton.setOnClickListener(this);
+		
+		mUserName = (EditText) findViewById(R.id.inputUserName);
+		mPassword = (EditText) findViewById(R.id.inputPassword);
 	}
 
 	@Override
@@ -35,24 +41,21 @@ public class JobSearch extends Activity implements OnClickListener{
 		return true;
 	}
 	
-	private class connectToWebsite extends AsyncTask <String, Void, String>{
+	private class connectToWebsite extends AsyncTask <String, Integer, String>{
 
 		@Override
 		protected String doInBackground(String... urls) {
 			String response = null;
-			for (String url : urls) {
-				Document doc = null;
-			    try {
-			    	doc = Jsoup.connect(url)
-			    			  .data("module", "student")
-			    			  .data("userName", "enana")
-			    			  .data("userPassword", "fUcktbs!1!")
-			    			  .post();
-				    response = doc.toString();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			Document doc = null;
+			try {
+			    doc = Jsoup.connect(urls[0])
+			    			.data("module", "student")
+			    			.data("userName", urls[1])
+			    			.data("userPassword", urls[2])
+			    			.post();
+				response = doc.toString();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 			return response;
 		}
@@ -62,19 +65,23 @@ public class JobSearch extends Activity implements OnClickListener{
 		    if (result.contains("Welcome")) {
 		    	Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
 		    }
-/*	      view = (TextView) findViewById(R.id.textView);
-	      view.setMovementMethod(new ScrollingMovementMethod());
-	      view.setText(result);*/
+		    else {
+		    	Toast.makeText(getApplicationContext(), "FAILED", Toast.LENGTH_SHORT).show();
+		    }
 	    }
 		
 	}
 
 	@Override
-	public void onClick(View v) {
-		switch(v.getId()) {
-			case R.id.button:{
+	public void onClick(View view) {
+		switch(view.getId()) {
+			case R.id.loginButton:{
+				String userName = mUserName.getText().toString();
+				String password = mPassword.getText().toString();
+				Log.w("js", "userName:  " + userName);
+				Log.w("password", "password:  " + password);
 				connectToWebsite connect = new connectToWebsite();
-				connect.execute(new String[] {LOGIN_URL});
+				connect.execute(new String[] {LOGIN_URL, userName, password});
 			}
 		}
 	}
